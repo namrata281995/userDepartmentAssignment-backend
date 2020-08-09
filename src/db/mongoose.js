e59a93ccff0419b6deef4899b0d904afb33b8393
Namrata <namrata@pratian.com>
2020-08-09 14:16:13 +0530

    mongoDB config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/userdepartmentapp',{
+// mongoose.connect('mongodb://127.0.0.1:27017/userdepartmentapp',{
+//     useNewUrlParser: true,
+//     useCreateIndex : true,
+//     useUnifiedTopology: true
+// })
+
+mongoose.connect('mongodb+srv://namrata:nami@28@cluster0.emtnv.mongodb.net/userdepartmentapp?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex : true,
     useUnifiedTopology: true
 })
+
+
